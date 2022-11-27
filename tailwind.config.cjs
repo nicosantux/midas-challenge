07089d86cda07fb1066bfa3b0c3349e69e91ec7d
Nicolás Santuccio <nico.santuccio@gmail.com>
2022-11-27 15:17:02 -0300

    dependencies: added dependencies and project configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
+  corePlugins: {
+    container: false,
   },
-  plugins: [],
+  theme: {
+    extend: {
+      screens: {
+        mobile: '320px',
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px',
+      },
+      colors: {
+        primary: colors.purple,
+        success: colors.green,
+        error: colors.red,
+        warning: colors.amber,
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        'loading-pulse': 'loading-pulse 1s linear infinite',
+      },
+      keyframes: {
+        'loading-pulse': {
+          '0%': { transform: 'scale(0); opacity: 1;' },
+          '100%': { transform: 'scale(1); opacity: 0' },
+        },
+      },
+    },
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: 'min(1920px, 90%)',
+          marginInline: 'auto',
+        },
+      })
+    },
+  ],
 }
