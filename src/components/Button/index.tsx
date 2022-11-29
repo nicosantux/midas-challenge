91b4cd6453bddfc3e4a57e4bece32ddd1dd2a544
Nicolás Santuccio <nico.santuccio@gmail.com>
2022-11-28 22:03:21 -0300

    components: updated button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
-import { type ButtonHTMLAttributes, forwardRef } from 'react'
+import type { FC, ButtonHTMLAttributes } from 'react'
+
 import { cva, VariantProps } from 'class-variance-authority'
 
 const buttonStyles = cva(
@@ -7,9 +8,12 @@ const buttonStyles = cva(
     variants: {
       variant: {
         primary:
-          'bg-primary-700 text-gray-50 px-4 h-12 hover:bg-primary-500 focus-visible:bg-primary-500',
-        secondary: 'border border-primary-700 hover:bg-primary-100 focus-visible:bg-primary-100',
+          'bg-primary-700 text-gray-50 px-4 h-12 hover:bg-primary-500 focus-visible:bg-primary-500 disabled:bg-primary-700/50',
+        secondary:
+          'border border-primary-700 hover:bg-primary-100 px-4 h-12 text-primary-700 focus-visible:bg-primary-100 disabled:border-primary-700/50 disabled:text-primary-700/50',
         gray: 'bg-gray-200/70 text-gray-800 hover:bg-gray-200 focus-visible:bg-gray-200',
+        error:
+          'bg-error-700 text-gray-50 px-4 h-12 hover:bg-error-500 focus-visible:bg-error-500 disabled:bg-error-700/50',
       },
       fullWidth: {
         true: 'w-full',
@@ -28,18 +32,21 @@ export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonStyles> {}
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
-  { children, className = '', variant, fullWidth, rounded, ...props },
-  ref,
-) {
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  variant,
+  fullWidth,
+  rounded,
+  ...props
+}) => {
   return (
     <button
       className={buttonStyles({ variant, fullWidth, rounded, className })}
-      ref={ref}
       type="button"
       {...props}
     >
       {children}
     </button>
   )
-})
+}
