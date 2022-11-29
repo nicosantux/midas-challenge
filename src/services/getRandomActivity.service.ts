717deac241cdd53897459265aafe5e667c038858
Nicolás Santuccio <nico.santuccio@gmail.com>
2022-11-29 11:54:33 -0300

    services: updated getRandomactivity to accept filters

--- src/services/getRandomActivity.service.ts
+++ src/services/getRandomActivity.service.ts
@@ -2,12 +2,12 @@ import type { Activity } from '@/types'
 
 const URL = 'http://www.boredapi.com/api/activity'
 
-export const getRandomActivity = async (): Promise<Activity> => {
-  const response = await fetch(URL)
+export const getRandomActivity = async (filterParams: string = ''): Promise<Activity> => {
+  const response = await fetch(`${URL}/?${filterParams}`)
 
   const data = await response.json()
 
-  if (data['error']) throw new Error(data)
+  if (data['error']) return Promise.reject(data.error)
 
   return data
 }
