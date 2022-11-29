4e462a5c139efb8b5f485fbc82f8303a9acec8e7
Nicolás Santuccio <nico.santuccio@gmail.com>
2022-11-29 09:58:05 -0300

    pages: completed not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,16 @@
 import type { FC } from 'react'
 
+import { Link } from '@/components'
+
 const NotFound: FC = () => {
-  return <div>Not Found</div>
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <span>404 | Not found</span>
+      <Link className="focus-ring font-bold hover:underline focus-visible:underline" to="/">
+        Go back
+      </Link>
+    </div>
+  )
 }
 
 export default NotFound
